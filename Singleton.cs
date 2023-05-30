c405c1a77326b7f346dab264ed2cc29fae052c79
NGUYỄN NGỌC TOÀN <83264177+NNT1208PT173@users.noreply.github.com>
2023-05-30 17:42:16 +0700

    Update Singleton.cs

--- Singleton.cs
+++ Singleton.cs
@@ -2,20 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    private static T _instance = null;
-    public static T Instance => _instance;
 
-    private void Awake()
+    private static T _instant = null;
+    public static T Instant
     {
-        if(_instance == null) {
-            _instance = this.GetComponent<T>();
-            return;
-        }
-        if(_instance.gameObject.GetInstanceID() != gameObject.GetInstanceID())
+        get
         {
-            Destroy(gameObject);
+            if (_instant == null)
+            {
+                if (FindObjectOfType<T>() != null)
+                    _instant = FindObjectOfType<T>();
+                else
+                    new GameObject().AddComponent<T>().name = "Singleton_" + typeof(T).ToString();
+            }
+
+            return _instant;
         }
+    }
+
+    void Awake()
+    {
+        if (_instant != null && _instant.gameObject.GetInstanceID() != this.gameObject.GetInstanceID())
+        {
+            Debug.LogError("Singleton already exist " + _instant.gameObject.name);
+            Destroy(this.gameObject);
+        }
+        else
+            _instant = this.GetComponent<T>();
     }
 }
