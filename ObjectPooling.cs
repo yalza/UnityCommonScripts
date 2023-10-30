36e906d4a4cce57d45b4e86006c770b5a0981de3
NGUYỄN NGỌC TOÀN <toann3764@gmail.com>
2023-10-30 22:45:35 +0700

    update observer

--- ObjectPooling.cs
+++ ObjectPooling.cs
@@ -1,34 +1,103 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPooling : Singleton<ObjectPooling>
+namespace DATA.Scripts.Core
 {
-    Dictionary<GameObject, List<GameObject>> _listObject = new Dictionary<GameObject, List<GameObject>>();
-
-    public GameObject GetGameObject(GameObject obj)
+    public class Observer : Singleton<Observer>
     {
-        if (_listObject.ContainsKey(obj))
+        private readonly Dictionary<string, List<Action>> _listeners = new Dictionary<string, List<Action>>();
+        private readonly Dictionary<string, List<Action<object>>> _listenersWithParam = new Dictionary<string, List<Action<object>>>();
+        public void RegisterObserver(string key, Action action)
         {
-            foreach (GameObject go in _listObject[obj])
+            List<Action> actions;
+            if (_listeners.TryGetValue(key, out var listener))
             {
-                if (go.activeSelf) continue;
-                return go;
+                actions = listener;
             }
-            GameObject g = Instantiate(obj, this.transform.position, Quaternion.identity);
-            _listObject[obj].Add(g);
-            g.SetActive(false);
+            else
+            {
+                actions = new List<Action>();
+                _listeners.Add(key, actions);
+            }
 
-            return g;
+            actions.Add(action);
+        }
+      
+        public void RegisterObserver(string key, Action<object> action)
+        {
+            List<Action<object>> actions;
+            if (_listenersWithParam.TryGetValue(key, out var listener))
+            {
+                actions = listener;
+            }
+            else
+            {
+                actions = new List<Action<object>>();
+                _listenersWithParam.Add(key, actions);
+            }
+
+            actions.Add(action);
+        }
+        
+        public void NotifyObservers(string key, object param)
+        {
+            if (_listenersWithParam.TryGetValue(key, out var listener))
+            {
+                foreach (Action<object> a in listener)
+                {
+                    try
+                    {
+                        a?.Invoke(param);
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.LogError(e);
+                    }
+                }
+            }
+            else
+            {
+                Debug.LogErrorFormat("listener {0} not exist", key);
+            }
         }
 
-        List<GameObject> list = new List<GameObject>();
-        GameObject g2 = Instantiate(obj, this.transform.position, Quaternion.identity);
-        list.Add(g2);
-        g2.SetActive(false);
-        _listObject.Add(obj, list);
+        public void NotifyObservers(string key)
+        {
+            if (_listeners.TryGetValue(key, value: out var listener))
+            {
+                foreach (Action a in listener)
+                {
+                    try
+                    {
+                        a?.Invoke();
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.LogError(e);
+                    }
+                }
+            }
+            else
+            {
+                Debug.LogErrorFormat("listener {0} not exist", key);
+            }
+        }
+        
+        public void RemoveObserver(string key, Action<object> action)
+        {
+            if (_listenersWithParam.TryGetValue(key, out var listener))
+            {
+                listener.Remove(action);
+            }
+        }
 
-        return g2;
-
+        public void RemoveObserver(string key, Action action)
+        {
+            if (_listeners.TryGetValue(key, out var listener))
+            {
+                listener.Remove(action);
+            }
+        }
     }
 }
